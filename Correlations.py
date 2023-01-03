a8d025919d168a8d518a261b512aa3f8774f4fb2
Artur Dobija <arturdobija@gmail.com>
2023-01-03 20:51:17 +0100

    Prepared change in the query: Correlations method run_query_...: changed the way kern file is processed. Prepared for quering every string in file. TODO: split the accumulated notes as chords in the single kern spine into separate spines (or find a different way to query each note in the chord token); Added folder 'query' where every files used for kern data transformation are stored; Removed redundant files.

--- Correlations.py
+++ Correlations.py
@@ -24,6 +24,8 @@ class Correlations_in_kern_repository:
 			correlated_bars = self.get_correlated_bars(f'{kern_repo_dir}/{piece}')
 			if correlated_bars:
 				self.results[piece] = correlated_bars
+
+		print("CHECKING FINISHED.")
 				
 	def report_correlation_for_piece (self, piece, results):
 		print('testikel')
@@ -50,19 +52,50 @@ class Correlations_in_kern_repository:
 		bars_as_myank_strings = self.get_bars_as_myank_strings(bar_numbers_with_high_correlations)
 		return bars_as_myank_strings
 
+	# def run_query_using_cmd_and_create_files_with_results (self, kern_piece_dir:str):
+
 	def run_query_using_cmd_and_create_files_with_results (self, kern_piece_dir:str):
+		# This version of the function uses less files
+		# and concatenates the whole sonata kern file
+		# into a single **kern spine.
+		# ! ! ! the chord from the spine should be split
+		# ! ! ! into separate spines in order for correl
+		# ! ! ! to give satisfactory results.
+		kern_only_dir = 'query/pieceKernOnly.kern'
+		one_spine_dir = 'query/pieceOneSpine.kern'
+		piece_semits_dir = 'query/piece.semits'
+		input_semits_dir = 'query/input.semits'
+		piece_correl_dir = 'query/piece.correl'
 		with\
 			open(kern_piece_dir, 'r') as file,\
-			open('input.semits', 'w') as input_semits,\
-			open('mozart.semits', 'w') as sonata_semits,\
-			open('sonataLeftSpine.semits', 'w') as sonata_left_spine_semits,\
-			open('sonataLeftColumn.semits', 'w') as sonata_left_column_semits,\
-			open('sonataLeftColumn.correl', 'w') as sonata_left_column_correl:
-			subprocess.run(['semits', '-x', self.input_file_name], stdout=input_semits)
-			subprocess.run(['semits', '-x', kern_piece_dir], stdout=sonata_semits)
-			subprocess.run(['extract', '-p', '1', 'mozart.semits'], stdout=sonata_left_spine_semits)
-			subprocess.run(['cut', '-f', '1', 'sonataLeftSpine.semits'], stdout=sonata_left_column_semits)
-			subprocess.run(['correl', '-s', '[\ \*\.r=]', '-f', 'input.semits', 'sonataLeftColumn.semits'], stdout=sonata_left_column_correl)		
+			open(kern_only_dir, 'w') as piece_kern_only,\
+			open(one_spine_dir, 'w') as piece_one_kern_spine,\
+			open(piece_semits_dir, 'w') as piece_semits,\
+			open(input_semits_dir, 'w') as input_semits,\
+			open(piece_correl_dir, 'w') as piece_correl:
+
+			subprocess.run(['extract','-i','**kern',kern_piece_dir], stdout=piece_kern_only) # use only **kern spines
+			# THIS IS WRONG: should contain -d " " but I have problem with this whitespace.
+			subprocess.run(['cleave','-i','**kern','-o','**kern',kern_only_dir], stdout=piece_one_kern_spine) # concatenate every **kern spines into one **kern spine containing chords only.
+
+			subprocess.run(['semits','-x',one_spine_dir], stdout=piece_semits) # turn notes into semits
+			subprocess.run(['semits','-x',self.input_file_name], stdout=input_semits) # make input file to semits. !!! WORTHWHILE to do this only once at the beginning.
+			subprocess.run(['correl','-s','[\ \*\.r=]','-f',input_semits_dir,piece_semits_dir], stdout=piece_correl)	
+		# ! ! ! P R E V I O U S ! ! !
+		# 	with\
+		# 		open(kern_piece_dir, 'r') as file,\
+		# 		open('input.semits', 'w') as input_semits,\
+		# 		open('mozart.semits', 'w') as sonata_semits,\
+		# 		open('sonataLeftSpine.semits', 'w') as sonata_left_spine_semits,\
+		# 		open('sonataLeftColumn.semits', 'w') as sonata_left_column_semits,\
+		# 		open('sonataLeftColumn.correl', 'w') as sonata_left_column_correl:
+		# 		subprocess.run(['semits', '-x', self.input_file_name], stdout=input_semits)
+		# 		subprocess.run(['semits', '-x', kern_piece_dir], stdout=sonata_semits)
+		# 		subprocess.run(['extract', '-p', '1', 'mozart.semits'], stdout=sonata_left_spine_semits)
+		# 		subprocess.run(['cut', '-f', '1', 'sonataLeftSpine.semits'], stdout=sonata_left_column_semits)
+		# 		subprocess.run(['correl', '-s', '[\ \*\.r=]', '-f', 'input.semits', 'sonataLeftColumn.semits'], stdout=sonata_left_column_correl)		
+		# ! ! !                 ! ! !
+
 
 	def get_bars_as_myank_strings (self, bar_numbers_with_high_correlations:list):
 		bars_as_myank_strings = []
@@ -89,8 +122,8 @@ class Correlations_in_kern_repository:
 	def get_correlated_bar_numbers (self):
 		bar_numbers_with_high_correlations = []
 		with\
-			open('sonataLeftColumn.semits') as sonata_left_column_semits,\
-			open('sonataLeftColumn.correl') as sonata_left_column_correl:
+			open('query/piece.semits') as sonata_left_column_semits,\
+			open('query/piece.correl') as sonata_left_column_correl:
 			semits_file = sonata_left_column_semits.readlines()
 			for i, line in enumerate(sonata_left_column_correl):
 				correlation:float = 0

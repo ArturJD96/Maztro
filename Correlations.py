ad095d65ec8025242e66e74d991ae05df86aebcb
Artur Dobija <arturdobija@gmail.com>
2023-01-10 23:43:54 +0100

    Starting refactoring the command line search for correlations. Implemented the newer method, but with old variable names.

--- Correlations.py
+++ Correlations.py
@@ -67,19 +67,38 @@ class Correlations_in_kern_repository:
 		# 	subprocess.run(['correl','-s','[\ \*\.r=]','-f',input_semits_dir,piece_semits_dir], stdout=piece_correl)	
 
 		#! ! ! P R E V I O U S ! ! !
-			with\
-				open(kern_piece_dir, 'r') as file,\
-				open('input.semits', 'w') as input_semits,\
-				open('mozart.semits', 'w') as sonata_semits,\
-				open('sonataLeftSpine.semits', 'w') as sonata_left_spine_semits,\
-				open('sonataLeftColumn.semits', 'w') as sonata_left_column_semits,\
-				open('sonataLeftColumn.correl', 'w') as sonata_left_column_correl:
-				subprocess.run(['semits', '-x', self.input_file_name], stdout=input_semits)
-				subprocess.run(['semits', '-x', kern_piece_dir], stdout=sonata_semits)
-				subprocess.run(['extract', '-p', '1', 'mozart.semits'], stdout=sonata_left_spine_semits)
-				subprocess.run(['cut', '-f', '1', 'sonataLeftSpine.semits'], stdout=sonata_left_column_semits)
-				subprocess.run(['correl', '-s', '[\ \*\.r=]', '-f', 'input.semits', 'sonataLeftColumn.semits'], stdout=sonata_left_column_correl)		
+			# with\
+			# 	open(kern_piece_dir, 'r') as file,\
+			# 	open('input.semits', 'w') as input_semits,\
+			# 	open('mozart.semits', 'w') as sonata_semits,\
+			# 	open('sonataLeftSpine.semits', 'w') as sonata_left_spine_semits,\
+			# 	open('sonataLeftColumn.semits', 'w') as sonata_left_column_semits,\
+			# 	open('sonataLeftColumn.correl', 'w') as sonata_left_column_correl:
+			# 	subprocess.run(['semits', '-x', self.input_file_name], stdout=input_semits)
+			# 	subprocess.run(['semits', '-x', kern_piece_dir], stdout=sonata_semits)
+			# 	subprocess.run(['extract', '-p', '1', 'mozart.semits'], stdout=sonata_left_spine_semits)
+			# 	subprocess.run(['cut', '-f', '1', 'sonataLeftSpine.semits'], stdout=sonata_left_column_semits)
+			# 	subprocess.run(['correl', '-s', '[\ \*\.r=]', '-f', 'input.semits', 'sonataLeftColumn.semits'], stdout=sonata_left_column_correl)		
 		#! ! !                 ! ! !
+
+		# H Y B R I D – leaving old names but using the new method.
+			with\
+				open('mozart_kern_only.krn', 'w') as piece_kern_only,\
+				open('mozart_one_kern_spine.krn', 'w') as piece_one_kern_spine,\
+				open('sonataLeftSpine.semits', 'w') as sonata_left_spine_semits,\
+				open('input.semits', 'w') as input_semits,\
+				open('sonataLeftColumn.correl', 'w') as sonata_left_column_correl:
+				# 1) remove all spines that are not **kern
+				subprocess.run(['extract','-i','**kern',kern_piece_dir], stdout=piece_kern_only)
+				# 2) concatenate every **kern spines into one **kern spine (containing chords only).
+				subprocess.run(['cleave','-d','" "','-i','**kern','-o','**kern','mozart_kern_only.krn'], stdout=piece_one_kern_spine)
+				# 3) turn each note into semits
+				subprocess.run(['semits','-x','mozart_one_kern_spine.krn'], stdout=sonata_left_spine_semits)
+				# 4) make input file to semits. !!! WORTHWHILE to do this only once at the beginning.
+				subprocess.run(['semits', '-x', self.input_file_name], stdout=input_semits)
+				# 5) look for correlation
+				subprocess.run(['correl', '-s', '[\ \*\.r=]', '-f', 'input.semits', 'sonataLeftColumn.semits'], stdout=sonata_left_column_correl)	
+
 
 	def drop_bars_being_too_close (self, bar_numbers_with_high_correlations):
 		for bar_number, correlation in bar_numbers_with_high_correlations:
@@ -122,7 +141,7 @@ class Correlations_in_kern_repository:
 					correlation = float(line.replace(',', '.'))
 				except ValueError:
 					pass
-				if correlation >= Correlations_in_kern_repository.CORRELATION_MINIMUM:	# negative ?
+				if correlation >= Correlations_in_kern_repository.CORRELATION_MINIMUM:	# or correlation is <= -Correlations_in_kern_repository.CORRELATION_MINIMUM
 					# append the BARNUMBER when a certain correlation is achieved
 					line_index = i
 					barline_found = False
